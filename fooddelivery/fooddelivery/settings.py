91cc133badfc3499b9b2229431bbaa7b1fa92576
Md Hasanuzzaman <dr.has82@gmail.com>
2024-03-26 01:17:30 +0600

    Url Mapping & template with base, index and about page created for customer

--- fooddelivery/fooddelivery/settings.py
+++ fooddelivery/fooddelivery/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'customer.apps.CustomerConfig',
+    'restaurant.apps.RestaurantConfig',
 ]
 
 MIDDLEWARE = [
